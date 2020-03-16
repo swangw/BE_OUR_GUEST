99a924c08355d3e81bb69ff227d1b2c738242561
AnnKoh <annkoh12@gmail.com>
2020-03-16 18:06:36 +0800

    Working on bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,22 @@
 class BookingsController < ApplicationController
   def index
-    @
+    @bookings = Booking.where(params[:host_id])
   end
 
   def show
 
+    @booking = Booking.find(params[:id])
+
   end
 
   def create
+    @space = Space.find(params[:space_id])
+    raise
+    @booking = Booking.new(booking_params)
+    @booking.space = @space
+
+    @booking.save
+
+
   end
 end

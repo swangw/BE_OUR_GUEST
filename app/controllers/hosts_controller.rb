9cdf70d60520bc036bc54e0f3877c7a37addadc5
AnnKoh <annkoh12@gmail.com>
2020-03-16 17:07:08 +0800

    Added Hosts controller

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,2 +1,18 @@
 class HostsController < ApplicationController
+  def new
+    @host = Host.new
+  end
+
+  def create
+    @host = Host.new(host_params)
+    @host.save
+
+    redirect_to host_spaces(@host)
+  end
+
+  private
+
+  def host_params
+    params.require(:host).permit(:email)
+  end
 end

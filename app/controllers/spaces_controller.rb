47c1c466a9bcdbe888b41a02bf4c95d6c04643bb
Saken <swangwio@gmail.com>
2020-03-17 00:31:22 -0400

    added spaces new

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -8,4 +8,25 @@ class SpacesController < ApplicationController
     @booking = Booking.new
   end
 
+  def new
+    @host = Host.find(params[:host_id])
+    @space = Space.new
+  end
+
+  def create
+    @host = Host.find(params[:host_id])
+    @space = Space.new(space_params)
+    @space.host = @host
+
+    if @space.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def space_params
+    params.require(:space).permit(:name, :address, :price_per_hour, :outlets, :capacity)
+  end
+
 end

b612527f62a85fd5337ea01736a58a6357415457
Saken <swangwio@gmail.com>
2020-03-17 03:04:28 -0400

    added index decision

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -30,6 +30,23 @@ class SpacesController < ApplicationController
     end
   end
 
+  def edit
+    @host = Host.find(params[:host_id])
+    @space = Space.find(params[:id])
+  end
+
+  def update
+    @host = Host.find(params[:host_id])
+    @space = Space.new(space_params)
+    @space.host = @host
+
+    if @space.save
+      redirect_to host_spaces_path
+    else
+      render :new
+    end
+  end
+
   def space_params
     params.require(:space).permit(:name, :address, :price_per_hour, :outlets, :capacity)
   end

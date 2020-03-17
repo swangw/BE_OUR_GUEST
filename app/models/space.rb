271a4341d2d5cfa9b658d7cd41c3b7cd66ffa429
Saken <swangwio@gmail.com>
2020-03-16 22:36:32 -0400

    added cloudinary

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,7 @@
 class Space < ApplicationRecord
   belongs_to :host
   has_many :bookings, dependent: :destroy
+  has_many_attached :photos
 
   validates :name, uniqueness: true, presence: true
   validates :address, uniqueness: true, presence: true

814aaec93a0386ee34f1561f7a2bc88e7bca3be6
Sarah Wester <softlytothedawn@gmail.com>
2020-03-18 10:41:10 +0800

    schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_093104) do
+ActiveRecord::Schema.define(version: 2020_03_17_071356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,8 +44,6 @@ ActiveRecord::Schema.define(version: 2020_03_17_093104) do
     t.bigint "space_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "host_id"
-    t.index ["host_id"], name: "index_bookings_on_host_id"
     t.index ["space_id"], name: "index_bookings_on_space_id"
   end
 
@@ -83,7 +81,6 @@ ActiveRecord::Schema.define(version: 2020_03_17_093104) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "hosts"
   add_foreign_key "bookings", "spaces"
   add_foreign_key "spaces", "hosts"
 end

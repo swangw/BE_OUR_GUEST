53f35e1422e1eb5bde6eeabea26b1d15d8a325e2
Saken <swangwio@gmail.com>
2020-03-19 22:55:20 -0400

    starting booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_035145) do
+ActiveRecord::Schema.define(version: 2020_03_20_021908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_03_18_035145) do
     t.bigint "space_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmed", default: false
     t.index ["space_id"], name: "index_bookings_on_space_id"
   end
 
